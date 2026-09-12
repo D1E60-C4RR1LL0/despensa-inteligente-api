d1020adfb6ad130b4df4d7a736e431da5ea4d1e8
Diego Carrillo G. <di.carrillog@duocuc.cl>
2026-09-12 01:44:40 -0300

    hotfix: excluir productos inactivos de alertas

--- tests/test_stocks.py
+++ tests/test_stocks.py
@@ -13,3 +13,23 @@ def test_read_stock_alertas(authorized_client, db_session, test_stock):
         stock["producto_id"] == test_stock.producto_id
         for stock in data
     )
+    
+def test_read_stock_alertas_no_incluye_productos_inactivos(
+    authorized_client,
+    db_session,
+    test_stock
+):
+    test_stock.cantidad = test_stock.producto.stock_minimo
+    test_stock.producto.activo = False
+    db_session.commit()
+
+    response = authorized_client.get("/stocks/alertas")
+
+    assert response.status_code == 200
+
+    data = response.json()
+
+    assert not any(
+        stock["producto_id"] == test_stock.producto_id
+        for stock in data
+    )    
